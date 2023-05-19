cae5852e20a242df17b7fea2d966bc0d002912a7
Andrew Pipo <atpipo2026@gmail.com>
2023-05-19 16:01:11 -0400

    Added Part I tasks for Lab 4

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,67 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // declaring variables as int
+        int intOperandA = 5;
+        int intOperandB = 4;
+        // the below variables should not be initialized with a value as it is seen as redundant and throws
+        // a warning, however lab instructions did not indicate such clarification.
+        int intSum = 1;
+        int intProduct = 2;
+        int intDifference = 3;
+        int intQuotient = 6;
+        int intModulo = 7;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // declaring variables as double
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 6.50;
+        // the below variables should not be initialized with a value as it is seen as redundant and throws
+        // a warning, however lab instructions did not indicate such clarification.
+        double doubleSum = 4.25;
+        double doubleProduct = 5.75;
+        double doubleDifference = -5.5;
+        double doubleQuotient = 3.14;
+
+        // Tasks performed using the int typed variables
+
+        intSum = intOperandA + intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum); // Output Statement
+
+        intProduct = intOperandA * intOperandB; // Assignment that uses the arithmetic operator *
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct); // Output Statement
+
+        intDifference = intOperandA - intOperandB; // Assignment that uses the arithmetic operator -
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference); // Output Statement
+
+        intQuotient = intOperandA / intOperandB; // Assignment that uses the arithmetic operator /
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient); // Output Statement
+
+        intModulo = intOperandA % intOperandB; // Assignment that uses the arithmetic operator %
+        System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo); // Output Statement
+
+        // Tasks performed using the double typed variables
+
+        doubleSum = doubleOperandA + doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum); // Output Statement
+
+        doubleProduct = doubleOperandA * doubleOperandB; // Assignment that uses the arithmetic operator *
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct); // Output Statement
+
+        doubleDifference = doubleOperandA - doubleOperandB; // Assignment that uses the arithmetic operator -
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference); // Output Statement
+
+        doubleQuotient = doubleOperandA / doubleOperandB; // Assignment that uses the arithmetic operator /
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient); // Output Statement
+
+        int kidsInMyFamily = 2;
+        String isRaining = "Yes";
+        double priceOfGas = 3.55;
+        int myFavoriteNumber = 7;
+        int myShoeSize = 15;
+        int myBirthMonth = 5;
+        String myFullName = "Andrew Thomas Pipo";
+
+
     }
 }
